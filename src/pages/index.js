25b15ddf45abd98d9a323c7c34b8af58a0030acf
KeviinCosmos <kc@keviincosmos.com>
2020-11-17 16:38:09 +0100

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from "react"
+import {Link} from "gatsby"
 
-export default function Home() {
-  return <div>
+
+
+const Home = () => (
+  <div>
     <h1>Hello world! :) </h1>
-    <p>Go to <a href="/test">test page here</a></p>
+    <p>Go to <Link to="/test/">test page here</Link></p>
+    <img src="https://cosmosco.dk/wp-content/uploads/2020/03/keviin-cosmos-isoleret.png.webp" width="200"/>
  </div>
-}
+)
+
+export default Home;
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const test = () => (
+const Test = () => (
     <div>
         <h1>Test page here</h1>
         <p>Go to back to <a href="/">the homepage</a></p>
     </div>
 )
 
-export default test;
+export default Test;
