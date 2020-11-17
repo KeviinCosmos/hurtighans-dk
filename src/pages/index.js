547459b3e1a52abf5c1bc9c0a12dd8037b0f50b4
KeviinCosmos <kc@keviincosmos.com>
2020-11-17 16:30:59 +0100

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Hello world! :) </h1>
+    <p>Go to <a href="/test">test page here</a></p>
+ </div>
 }
---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const test = () => (
+    <div>
+        <h1>Test page here</h1>
+        <p>Go to back to <a href="/">the homepage</a></p>
+    </div>
+)
+
+export default test;
